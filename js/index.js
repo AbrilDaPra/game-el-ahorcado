5b66b11d91028b53216d269f08d228c66d76b5e4
Abril Da Pra <abydapra@gmail.com>
2023-08-10 14:29:56 -0300

    Hago array de categorias con objetos dentro con palabras

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let categories = [
+    {categoria: "Literatura", palabras: ["thriller", "biblioteca", "novela", "mujercitas", "frankenstein"]},
+    {categoria: "Transporte", palabras: ["colectivo", "tren", "automovil", "avion", "camiones"]},
+    {categoria: "Colores", palabras: ["violeta", "rojo", "amarillo", "azul", "verde agua"]},
+    {categoria: "Tecnologia", palabras: ["computadora", "teclado", "celular", "aspiradora", "microondas"]},
+    {categoria: "Animales", palabras: ["perro", "gato", "elefante", "tigre", "pájaro"]},
+    {categoria: "Ropa", palabras: ["remera", "pantalon", "buzo", "campera", "pantalones cortos"]},
+    {categoria: "Familia", palabras: ["madre", "padre", "abuelos", "tio", "primos"]},
+    {categoria: "Deportes", palabras: ["football", "basketball", "hockey", "voleyball", "natacion"]},
+    {categoria: "Peliculas", palabras: ["la bella durmiente", "matilda", "winnie pooh", "barbie", "volt"]},
+    {categoria: "Medicina", palabras: ["medico", "sanatorio", "febricula", "receta", "antibiotico"]}
+];
