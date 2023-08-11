8e4fb7200781601316f05fff745291d362a5af2e
Abril Da Pra <abydapra@gmail.com>
2023-08-11 11:29:03 -0300

    Agrego las letras incorrectas a su div correspondiente

--- js/index.js
+++ js/index.js
@@ -1,12 +1,65 @@
 let categories = [
-    {categoria: "Literatura", palabras: ["thriller", "biblioteca", "novela", "mujercitas", "frankenstein"]},
-    {categoria: "Transporte", palabras: ["colectivo", "tren", "automovil", "avion", "camiones"]},
-    {categoria: "Colores", palabras: ["violeta", "rojo", "amarillo", "azul", "verde agua"]},
-    {categoria: "Tecnologia", palabras: ["computadora", "teclado", "celular", "aspiradora", "microondas"]},
-    {categoria: "Animales", palabras: ["perro", "gato", "elefante", "tigre", "pájaro"]},
-    {categoria: "Ropa", palabras: ["remera", "pantalon", "buzo", "campera", "pantalones cortos"]},
-    {categoria: "Familia", palabras: ["madre", "padre", "abuelos", "tio", "primos"]},
-    {categoria: "Deportes", palabras: ["football", "basketball", "hockey", "voleyball", "natacion"]},
-    {categoria: "Peliculas", palabras: ["la bella durmiente", "matilda", "winnie pooh", "barbie", "volt"]},
-    {categoria: "Medicina", palabras: ["medico", "sanatorio", "febricula", "receta", "antibiotico"]}
+    {categorie: "Literatura", words: ["thriller", "biblioteca", "novela", "mujercitas", "frankenstein"]},
+    {categorie: "Transporte", words: ["colectivo", "tren", "automovil", "avion", "camiones"]},
+    {categorie: "Colores", words: ["violeta", "rojo", "amarillo", "azul", "verde agua"]},
+    {categorie: "Tecnologia", words: ["computadora", "teclado", "celular", "aspiradora", "microondas"]},
+    {categorie: "Animales", words: ["perro", "gato", "elefante", "tigre", "pájaro"]},
+    {categorie: "Ropa", words: ["remera", "pantalon", "buzo", "campera", "pantalones cortos"]},
+    {categorie: "Familia", words: ["madre", "padre", "abuelos", "tio", "primos"]},
+    {categorie: "Deportes", words: ["football", "basketball", "hockey", "voleyball", "natacion"]},
+    {categorie: "Peliculas", words: ["la bella durmiente", "matilda", "winnie pooh", "barbie", "volt"]},
+    {categorie: "Medicina", words: ["medico", "sanatorio", "febricula", "receta", "antibiotico"]}
 ];
+
+let randomIndex = Math.floor(Math.random() * categories.length);
+let randomCategorie = categories[randomIndex];
+let randomWordIndex = Math.floor(Math.random() * randomCategorie.words.length);
+let randomWord = randomCategorie.words[randomWordIndex];
+
+let elementCategorie = document.getElementById("categorie");
+elementCategorie.querySelector("p").textContent = randomCategorie.categorie;
+
+let elementWords = document.getElementById("words");
+elementWords.innerHTML = "";
+
+for (let i = 0; i < randomWord.length; i++) {
+    elementWords.innerHTML += "<span>_</span>";
+}
+
+let guessedLetters = new Set();
+let elementKeyboard = document.getElementById("keyboard");
+
+elementKeyboard.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        let letter = event.target.value;
+        
+        if (randomWord.includes(letter)) {
+            guessedLetters.add(letter);
+
+            for (let i = 0; i < randomWord.length; i++) {
+                if (randomWord[i] === letter) {
+                    elementWords.children[i].textContent = letter;
+                }
+            }
+        }
+    }
+});
+
+let elementIncorrectLetters = document.getElementById("incorrectLetters");
+
+elementKeyboard.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        let letter = event.target.value;
+
+        if (!randomWord.includes(letter) && !guessedLetters.has(letter)) {
+            guessedLetters.add(letter);
+            elementIncorrectLetters.textContent += letter + " ";
+        } else if (randomWord.includes(letter)) {
+            for (let i = 0; i < randomWord.length; i++) {
+                if (randomWord[i] === letter) {
+                    elementWords.children[i].textContent = letter;
+                }
+            }
+        }
+    }
+});
